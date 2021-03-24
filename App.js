66490f3e6ec7fb135922c97e6d9873a18509eb75
jonaujo89 <jonaujo89@gmail.com>
2021-03-24 00:46:58 -0400

    setting up eslint, prettier. adding react-native-paper

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+
+// const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchContainer}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.contentContainer}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  searchContainer: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  contentContainer: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
   },
 });
