ddbf0cb4bb32f8dfe83f84a76e9d567afff8d33f
jonaujo89 <jonaujo89@gmail.com>
2021-04-01 01:16:09 -0400

    saving/loading favorites from/to the phone. fixing android bug with compact view for favorites

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -2,7 +2,7 @@ import React from "react";
 import CompactRestaurantInfo from "../../../components/restaurant/compact-restaurant-info";
 
 const MapCallout = ({ restaurant }) => (
-  <CompactRestaurantInfo restaurant={restaurant} />
+  <CompactRestaurantInfo restaurant={restaurant} isMap />
 );
 
 export default MapCallout;
--- src/services/favorites/favorites.context.js
+++ src/services/favorites/favorites.context.js
@@ -1,9 +1,30 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavoritesContext = createContext();
 
 export const FavoritesContextProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
+
+  const saveFavorites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favorites", jsonValue);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const loadFavorites = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@favorites");
+      if (value !== null) {
+        setFavorites(JSON.parse(value));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const add = (restaurant) => {
     setFavorites([...favorites, restaurant]);
@@ -16,6 +37,14 @@ export const FavoritesContextProvider = ({ children }) => {
 
     setFavorites(newFavorites);
   };
+
+  useEffect(() => {
+    loadFavorites(favorites);
+  }, []);
+
+  useEffect(() => {
+    saveFavorites(favorites);
+  }, [favorites]);
 
   return (
     <FavoritesContext.Provider
