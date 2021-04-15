1c6993bff24b6ea0a7f91eb5023ee9725f3cf70e
jonaujo89 <jonaujo89@gmail.com>
2021-04-14 21:56:55 -0400

    connecting to geocode API with google functions and firebase

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,24 @@
 const { locations: locationsMock } = require("./geocode.mock");
-// const url = require("url");
+const functions = require("firebase-functions");
 
-module.exports.geocodeRequest = (request, response) => {
-  // const { city } = url.parse(request.url, true).query;
-  const { city } = request.query;
-  const locationMock = locationsMock[city.toLowerCase()];
-  response.json(locationMock);
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = request.query;
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    return response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().google.key
+      },
+      timeout: 1000
+    })
+    .then((res) => response.json(res.data))
+    .catch((error) => {
+      response.status(400);
+      return response.send(error.response.data.error_message);
+    });
 };
